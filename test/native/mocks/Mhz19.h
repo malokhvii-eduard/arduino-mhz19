3d9dc6aa2d6156a512e6432d88cb9b22021aea96
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-11-26 22:17:23 +0200

    test(api): add some mocks

---
+++ test/native/mocks/Mhz19.h
@@ -0,0 +1,19 @@
+#ifndef ARDUINO_MHZ19_TEST_NATIVE_MOCKS_MHZ19_H_
+#define ARDUINO_MHZ19_TEST_NATIVE_MOCKS_MHZ19_H_
+
+/* Google Test */
+#include <gmock/gmock.h>
+
+/* Library */
+#include <Mhz19.h>
+
+class Mhz19Mock : public Mhz19 {
+ public:
+  MOCK_METHOD(bool, readPacket, (const uint8_t, uint8_t*), (const, override));
+  MOCK_METHOD(void, writePacket,
+              (const uint8_t, const uint8_t, const uint8_t, const uint8_t,
+               const uint8_t, const uint8_t),
+              (const, override));
+};
+
+#endif  // ARDUINO_MHZ19_TEST_NATIVE_MOCKS_MHZ19_H_
